12f1ae1a4400e437f17460deced8768f84d4ca6d
Csilla Bukki <csillabukki@csillas-air.lan>
2024-08-01 16:17:55 -0700

    jobly frontend

---
+++ build/precache-manifest.97fbce29d46c6b92d09c02341864be7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02cf82c09e556c80eab6",
+    "url": "/static/css/main.c3ef3ba0.chunk.css"
+  },
+  {
+    "revision": "02cf82c09e556c80eab6",
+    "url": "/static/js/main.02cf82c0.chunk.js"
+  },
+  {
+    "revision": "314815fffb7e68cf59a0",
+    "url": "/static/css/1.11e9b510.chunk.css"
+  },
+  {
+    "revision": "314815fffb7e68cf59a0",
+    "url": "/static/js/1.314815ff.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8bfadecc77319261349acd6c172dcbb1",
+    "url": "/index.html"
+  }
+];
